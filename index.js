312aed5df102f22b5a2a18c4c7154ab9bf17d711
Steve Benchik <stevebenchik@gmail.com>
2017-05-15 11:49:48 -0400

    Show all details on form submit

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
 const personForm = document.querySelector('form')
 
-personForm.onsubmit = (ev) => {
+const handleSubmit = (ev) => {
     ev.preventDefault()
     const form = ev.target
     const details = document.querySelector('.details')
     const personName = form.personName.value
-
+    const hairColor = form.hairColor.value
+    const age = form.age.value
+    const birthplace = form.birthplace.value
     // details.innerHTML = `<em>${personName}</em>`
 
-    const em = document.createElement('em')
-    em.textContent = personName
-    details.appendChild(em)
+    const colorDiv = `<div style ="height: 50px; width: 100px; background-color: ${hairColor}"></div>`
+
+    const detailsList = document.createElement('ul')
+    details.appendChild(detailsList)
+
+    const nameLi = document.createElement('li')
+    nameLi.innerHTML = `Name: ${personName}`
+    detailsList.appendChild(nameLi)
+
+    const colorLi = document.createElement('li')
+    colorLi.innerHTML = colorDiv
+    detailsList.appendChild(colorLi)
+
+    const ageLi = document.createElement('li')
+    ageLi.innerHTML = `Age: ${age}`
+    detailsList.appendChild(ageLi)
+
+    const birthplaceLi = document.createElement('li')
+    birthplaceLi.innerHTML = `Birthplace: ${birthplace}`
+    detailsList.appendChild(birthplaceLi)
 }
+
+personForm.onsubmit = handleSubmit()
