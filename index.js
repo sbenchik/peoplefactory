b20402ea47bd355fadc3223d3230f7810350787b
Steve Benchik <stevebenchik@gmail.com>
2017-05-16 11:30:53 -0400

    refactor index.js to use Array.map

--- index.js
+++ index.js
@@ -1,45 +1,43 @@
 const personForm = document.querySelector('form')
 
-const createListItem = (htmlText, parentElement) => {
+const renderListItem = (name, value) => {
     const newListItem = document.createElement('li')
-    newListItem.textContent = htmlText
-    parentElement.appendChild(newListItem)
+    newListItem.textContent = `${name}: ${value}`
+    return newListItem
 }
 
-const renderColor = (htmlText, parentElement, hairColor) => {
-    createListItem(htmlText, parentElement) 
+const renderColor = (hairColor) => {
     const colorDiv = document.createElement('div')
-    parentElement.appendChild(colorDiv)
     colorDiv.style.height = '50px'
     colorDiv.style.width = '100px'
     colorDiv.style.backgroundColor = hairColor
+    return colorDiv
+}
+
+const renderList = (person) => {
+    const list = document.createElement('ul')
+    Object.keys(person).map((fieldName) => {
+        let li = renderListItem(fieldName, person[fieldName])
+        list.appendChild(li)
+    })
+    return list
 }
 
 const handleSubmit = (ev) => {
     ev.preventDefault()
     const form = ev.target
     const details = document.querySelector('.details')
-    const personName = form.personName.value
-    const hairColor = form.hairColor.value
-    const age = form.age.value
-    const birthplace = form.birthplace.value
-
-    const colorDiv = `<div style ="height: 50px; width: 100px; background-color: ${hairColor}"></div>`
-
-    const detailsList = document.createElement('ul')
-    details.appendChild(detailsList)
-
-    const inputs = document.querySelectorAll('input')
-    const labels = document.querySelectorAll('label')
-    let labelCount = 0
-    for(let i of inputs){
-        if(labels[labelCount].htmlFor != 'hairColor'){
-            createListItem(`${labels[labelCount].textContent}: ${i.value}`, detailsList)
-        } else {
-            renderColor(`${labels[labelCount].textContent}:`, detailsList, `${hairColor}`)
-        }
-        labelCount++
+    
+    const person = {
+        name: form.person.name,
+        hairColor: form.hairColor.value,
+        age: form.age.value,
+        birthplace: form.birthplace.value,
     }
+
+    const list = renderList(person)
+
+    details.appendChild(list)
 }
 
 personForm.addEventListener('submit', handleSubmit)
